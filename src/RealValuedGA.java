3dd3aa6ed5de88adf77ad43f19c642dbd2b7be70
oeg8168 <oeg8168@gmail.com>
2015-04-22 21:14:12 +0800

    Add mutate function

--- src/RealValuedGA.java
+++ src/RealValuedGA.java
@@ -8,6 +8,25 @@ public class RealValuedGA {
 	public static double crossoverRate = 0.80;
 	public static double mutationRate = 0.10;
 
+	/**
+	 * Do mutation at x or y or both
+	 * 
+	 * @param input
+	 *            - Individual
+	 * @return mutated Individual
+	 */
+	public static Individual mutate(Individual input) {
+		int xy = (int) (Math.random() * 3);
+
+		if (xy == 0) { // mutate all
+			return new Individual();
+		} else if (xy == 1) { // mutate x
+			return new Individual(Math.random() * GA_HW2.getRange() + GA_HW2.lower, input.getY());
+		} else { // mutate y
+			return new Individual(input.getX(), Math.random() * GA_HW2.getRange() + GA_HW2.lower);
+		}
+	} // end of mutation()
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
