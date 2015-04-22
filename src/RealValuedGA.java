e902bfbc607504522ad9fb356d2cb087dba94bce
oeg8168 <oeg8168@gmail.com>
2015-04-22 21:13:47 +0800

    Class for real valued GA

---
+++ src/RealValuedGA.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author LISM_OEG
+ *
+ */
+public class RealValuedGA {
+
+	public static double crossoverRate = 0.80;
+	public static double mutationRate = 0.10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+} // end of class RealValuedGA
